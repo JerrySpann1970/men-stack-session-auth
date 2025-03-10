afbaa595da784d4e1ea991b44686b49af3acbcbd
jerryspann <jerryspann0@gmail.com>
2025-03-10 10:36:34 -0700

    push missing elements

---
+++ server.js
@@ -0,0 +1,18 @@
+// dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+// init express app
+
+// configure settings
+
+// connect to mongoDB
+
+// mount middleware
+
+// mount routes
+
+// tell the app to listen
